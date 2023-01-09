8185a0d509d6b56bd2402429a4e3dd472f7e46e0
Muhammad Alfareza01~ <rezaalfa335@gmail.com>
2023-01-10 05:22:37 +0800

    ini adalah update

--- src/config/redux/action/createBlogAction.js
+++ src/config/redux/action/createBlogAction.js
@@ -1,6 +1,6 @@
 import axios from "axios" //npm untuk API
 
-const url = 'https://rz-app.adaptable.app/' 
+const url = 'https://rz-api.cyclic.app/' 
 
 export const setForm = (formType, formValue) =>{
 return {type: 'SET_FORM_DATA', formType, formValue}
